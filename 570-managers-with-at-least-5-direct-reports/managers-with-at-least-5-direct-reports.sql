212b248bb4b3af657eb7aa52e53bebafd9a0a581
Amit Panda <30612251+amitpanda365@users.noreply.github.com>
2024-03-07 22:03:13 +0530

    Time: 578 ms (74.42%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,7 +1,6 @@
-/* Write your T-SQL query statement below */
-select t1.name from employee t1 inner join
-(select e.id as id, count(1) as cnt from 
+# Write your MySQL query statement below
+
+select e.name from 
 employee e inner join employee m
 on e.id = m.managerId 
-group by e.id having count(1) >= 5) t2
-on t1.id = t2.id;
+group by e.id having count(1) >= 5;
