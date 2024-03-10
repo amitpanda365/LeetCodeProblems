bee4bf712b540053d91c27bfe7327a40afeb691c
Amit Panda <30612251+amitpanda365@users.noreply.github.com>
2024-03-10 11:36:07 +0530

    Time: 1607 ms (10.84%) | Memory: 0B (100.00%) - LeetSync

--- 1317-monthly-transactions-i/monthly-transactions-i.sql
+++ 1317-monthly-transactions-i/monthly-transactions-i.sql
@@ -1,17 +1,8 @@
-select t1.month, t1.country, t1.trans_count,
-case  when t1.country is null then t1.trans_count 
-when t2.approved_count is null then 0 else t2.approved_count end as 'approved_count',
-t1.trans_total_amount,
-case when t1.country is null then t1.trans_total_amount 
-when t2.approved_total_amount is null then 0 else t2.approved_total_amount end as 'approved_total_amount' from 
-(select DATE_FORMAT(trans_date, '%Y-%m') as month, country, 
-count(state) as trans_count, sum(amount) as trans_total_amount
+select DATE_FORMAT(trans_date, '%Y-%m') as month, 
+country, 
+count(state) as trans_count, 
+count(if(state='approved', 1, null)) as approved_count,
+sum(amount) as trans_total_amount,
+sum(if(state='approved', amount, 0)) as approved_total_amount
 from transactions
-group by month, country) t1 
-left join 
-(select DATE_FORMAT(trans_date, '%Y-%m') as month, country, 
-count(state) as approved_count, sum(amount) as approved_total_amount
-from transactions
-where state = 'approved'
-group by month, country) t2
-on t1.month = t2.month and t1.country = t2.country;
+group by month, country;
